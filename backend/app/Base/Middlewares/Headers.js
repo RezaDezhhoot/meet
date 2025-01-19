328bc22ef551b900d7e77cbf4a8fb900d1e8ac61
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-01-19 10:27:11 +0330

    fi bugs

--- backend/app/Base/Middlewares/Headers.js
+++ backend/app/Base/Middlewares/Headers.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 exports.Headers = (req, res, next) => {
+    console.log(process.env.FRONTEND_URL)
     res.set(cors({
         origin: process.env.FRONTEND_URL,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
